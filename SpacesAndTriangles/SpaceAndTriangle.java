f6a05cd79b323f91bd1dc4edac64424589f700d3
Aditya Ashtikar <ashtikar.aditya97@gmail.com>
2018-03-10 21:26:25 +0530

    second commit

--- SpacesAndTriangles/SpaceAndTriangle.java
+++ SpacesAndTriangles/SpaceAndTriangle.java
@@ -1,18 +1,8 @@
 class SpaceAndTriangle {
 
   public static void main(String args[]){
-    int i=1, j=1, k=1;
-    for(i=1; i<=5; i++){
-      for(j=1; j<=5; j++){
-        if(i!=5)
-          System.out.print(" ");
-        if(j == i){
-          for(k=1; k<=i; k++){
-            System.out.print(i);
-          }
-        }
-      }
-      System.out.println();
-    }
+
+    
+
   }
 }
