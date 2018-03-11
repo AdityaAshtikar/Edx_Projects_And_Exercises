3cb3b48db6b2874886966d540463804367fc1eea
Aditya Ashtikar <ashtikar.aditya97@gmail.com>
2018-03-11 14:26:18 +0530

    SpacesAndTriangles solved

--- SpacesAndTriangles/SpaceAndTriangle.java
+++ SpacesAndTriangles/SpaceAndTriangle.java
@@ -2,7 +2,17 @@ class SpaceAndTriangle {
 
   public static void main(String args[]){
 
-    
+    int i=1, j=1, k=1;
+
+    for(i=1; i<=5; i++){
+      for(j=5; j>i; j--){
+        System.out.print(" ");
+      }
+      for(k=1; k<=i; k++){
+        System.out.print(i);
+      }
+      System.out.println();
+    }
 
   }
 }
